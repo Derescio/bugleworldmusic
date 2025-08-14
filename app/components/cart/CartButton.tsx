2a98c54628aee0bc1a3ba4264d7be1f63b040bee
Derescio <ddw.web.dev.services@gmail.com>
2025-08-14 19:32:48 -0400

    Adjusted for Cart Icon

--- app/components/cart/CartButton.tsx
+++ app/components/cart/CartButton.tsx
@@ -14,7 +14,7 @@ export default function CartButton() {
       onClick={toggleCart}
       className="relative border-white/20 text-white hover:bg-white/10"
     >
-      <ShoppingCart className="h-4 w-4 text-black" />
+      <ShoppingCart className="h-4 w-4 " />
       {itemCount > 0 && (
         <span className="absolute -top-2 -right-2 bg-orange-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center font-bold">
           {itemCount > 99 ? '99+' : itemCount}
